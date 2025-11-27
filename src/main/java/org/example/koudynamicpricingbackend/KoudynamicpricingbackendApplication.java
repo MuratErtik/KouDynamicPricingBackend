3163279abedefbe4b7c75367e65108c0d3f92d90
Murat Ertik <dmuratertik1@gmail.com>
2025-11-27 03:03:32 -0800

    first commit.

---
+++ src/main/java/org/example/koudynamicpricingbackend/KoudynamicpricingbackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.koudynamicpricingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoudynamicpricingbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoudynamicpricingbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/koudynamicpricingbackend/KoudynamicpricingbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.koudynamicpricingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoudynamicpricingbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
