73cdd4f93696a1e0d35f4b30573562bcfaeef523
Murat Ertik <dmuratertik1@gmail.com>
2025-11-28 12:10:55 -0800

    exist admin role method has been added.

--- src/main/java/org/example/koudynamicpricingbackend/repositories/UserRepository.java
+++ src/main/java/org/example/koudynamicpricingbackend/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package org.example.koudynamicpricingbackend.repositories;
 
+import org.example.koudynamicpricingbackend.domains.UserRole;
 import org.example.koudynamicpricingbackend.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+
+    boolean existsByUserRole(UserRole userRole);
 }
