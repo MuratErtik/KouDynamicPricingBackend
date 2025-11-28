987e3dad20fc7767bdb81fc71f72c4f932344a62
Murat Ertik <dmuratertik1@gmail.com>
2025-11-28 10:34:42 -0800

    User Repo has been added.

---
+++ src/main/java/org/example/koudynamicpricingbackend/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package org.example.koudynamicpricingbackend.repositories;
+
+import org.example.koudynamicpricingbackend.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
