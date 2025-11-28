dba1bf1fb5fd918b935e2355182e6d86dbd0d939
Murat Ertik <dmuratertik1@gmail.com>
2025-11-28 12:44:18 -0800

    builder annotation has been added.

--- src/main/java/org/example/koudynamicpricingbackend/responses/AuthResponse.java
+++ src/main/java/org/example/koudynamicpricingbackend/responses/AuthResponse.java
@@ -1,8 +1,10 @@
 package org.example.koudynamicpricingbackend.responses;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class AuthResponse {
 
     private String accessToken;
