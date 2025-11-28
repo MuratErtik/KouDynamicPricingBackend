507f264bfa2b1c5b71588be3c7357d8fe0e3cdeb
Murat Ertik <dmuratertik1@gmail.com>
2025-11-28 12:13:00 -0800

    handleValidationExceptions has been added.

--- src/main/java/org/example/koudynamicpricingbackend/exceptions/GlobalException.java
+++ src/main/java/org/example/koudynamicpricingbackend/exceptions/GlobalException.java
@@ -2,11 +2,15 @@ package org.example.koudynamicpricingbackend.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalException {
@@ -24,6 +28,26 @@ public class GlobalException {
 
         return new ResponseEntity<>(errorDetail, HttpStatus.BAD_REQUEST);
 
+    }
+
+    //If @Valid gets to fail then this method gonna execute
+    //return to frontend which fields have error
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        // iterate to all fields which got errors and insert map like "email":"invalid format"
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+
+            String fieldName = ((FieldError) error).getField();
+
+            String errorMessage = error.getDefaultMessage();
+
+            errors.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
 }
