4db8b2fea3af21d76c35a7313ab875fc351e9951
Murat Ertik <dmuratertik1@gmail.com>
2025-11-29 15:32:50 -0800

    CORS

--- src/main/java/org/example/koudynamicpricingbackend/configs/SecurityConfig.java
+++ src/main/java/org/example/koudynamicpricingbackend/configs/SecurityConfig.java
@@ -66,10 +66,14 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*")); // dev only
+
+        config.setAllowedOriginPatterns(List.of("*"));
+
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
+
         config.setAllowCredentials(true);
+
         config.setExposedHeaders(List.of("Authorization"));
         config.setMaxAge(3600L);
 
