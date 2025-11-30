c3050f302498b54b7eab64ab1c9d0b377aa07593
Murat Ertik <dmuratertik1@gmail.com>
2025-11-30 04:20:29 -0800

    airport controller has been created.

---
+++ src/main/java/org/example/koudynamicpricingbackend/contollers/AirportController.java
@@ -0,0 +1,26 @@
+package org.example.koudynamicpricingbackend.contollers;
+
+import lombok.RequiredArgsConstructor;
+import org.example.koudynamicpricingbackend.responses.AirportResponse;
+import org.example.koudynamicpricingbackend.services.AirportService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/public/airport")
+@RequiredArgsConstructor
+public class AirportController {
+
+    private final AirportService airportService;
+
+    @GetMapping("/get-all")
+    public ResponseEntity<List<AirportResponse>> getAllAirports() {
+        return ResponseEntity.ok(airportService.getAllAirports());
+    }
+
+
+}
