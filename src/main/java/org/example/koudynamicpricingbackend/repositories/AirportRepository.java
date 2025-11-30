c1970e43e97c9924d0be5125c5868b448d2002e8
Murat Ertik <dmuratertik1@gmail.com>
2025-11-30 13:49:58 -0800

    find by id method enabled.

--- src/main/java/org/example/koudynamicpricingbackend/repositories/AirportRepository.java
+++ src/main/java/org/example/koudynamicpricingbackend/repositories/AirportRepository.java
@@ -3,5 +3,9 @@ package org.example.koudynamicpricingbackend.repositories;
 import org.example.koudynamicpricingbackend.entities.Airport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AirportRepository extends JpaRepository<Airport, Long> {
+
+    Optional<Airport> findAirportById(Long id);
 }
