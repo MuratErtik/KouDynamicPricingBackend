f275fc23ea1594b11a226ccd74eb1aa8ce861b85
Murat Ertik <dmuratertik1@gmail.com>
2025-11-30 14:11:22 -0800

    AdminFlightController has been completed.

---
+++ src/main/java/org/example/koudynamicpricingbackend/contollers/AdminFlightController.java
@@ -0,0 +1,29 @@
+package org.example.koudynamicpricingbackend.controllers;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.koudynamicpricingbackend.requests.CreateFlightRequest;
+import org.example.koudynamicpricingbackend.responses.CreateFlightResponse;
+import org.example.koudynamicpricingbackend.services.FlightService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/admin/flights")
+@RequiredArgsConstructor
+public class AdminFlightController {
+
+    private final FlightService flightService;
+
+    @PostMapping("/add")
+    public ResponseEntity<CreateFlightResponse> createFlight(@RequestHeader("Authorization") String jwt,
+                                                             @Valid @RequestBody CreateFlightRequest request) {
+
+        CreateFlightResponse response = flightService.createFlight(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+}
