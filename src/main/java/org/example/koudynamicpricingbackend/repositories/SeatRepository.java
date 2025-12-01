027caefcbaa35df9a78b6f70e465ee997b961229
Murat Ertik <dmuratertik1@gmail.com>
2025-12-01 01:48:15 -0800

    delete flight has been fixed.

--- src/main/java/org/example/koudynamicpricingbackend/repositories/SeatRepository.java
+++ src/main/java/org/example/koudynamicpricingbackend/repositories/SeatRepository.java
@@ -3,5 +3,11 @@ package org.example.koudynamicpricingbackend.repositories;
 import org.example.koudynamicpricingbackend.entities.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SeatRepository extends JpaRepository<Seat, Long> {
+
+    List<Seat> findByFlightIdOrderByIdAsc(Long flightId);
+
+    void deleteByFlightId(Long flightId);
 }
