d6c298d0269127d049d4d605ea1710d4ddb2ffaf
Murat Ertik <dmuratertik1@gmail.com>
2025-12-02 05:21:05 -0800

    specification added.

--- src/main/java/org/example/koudynamicpricingbackend/repositories/FlightRepository.java
+++ src/main/java/org/example/koudynamicpricingbackend/repositories/FlightRepository.java
@@ -2,6 +2,7 @@ package org.example.koudynamicpricingbackend.repositories;
 
 import org.example.koudynamicpricingbackend.entities.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface FlightRepository extends JpaRepository<Flight, Long> {
+public interface FlightRepository extends JpaRepository<Flight, Long>, JpaSpecificationExecutor<Flight> {
 }
