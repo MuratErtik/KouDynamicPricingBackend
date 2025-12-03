65f25589f0ad659d917e8350e64f2b994c581b52
Murat Ertik <dmuratertik1@gmail.com>
2025-12-03 11:08:46 -0800

    new parameter added.

---
+++ src/main/java/org/example/koudynamicpricingbackend/entities/SpecialDay.java
@@ -0,0 +1,33 @@
+package org.example.koudynamicpricingbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "special_days")
+public class SpecialDay {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDate endDate;
+
+
+    @Column(name = "price_multiplier", nullable = false)
+    private Double priceMultiplier;
+}
