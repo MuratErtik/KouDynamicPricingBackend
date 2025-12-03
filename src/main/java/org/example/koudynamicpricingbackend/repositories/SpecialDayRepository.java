f1561b91665b0f1ff6b7fb8500ec1755c0dad893
Murat Ertik <dmuratertik1@gmail.com>
2025-12-03 12:49:31 -0800

    repo created and query implemented for getting price multiplier.

---
+++ src/main/java/org/example/koudynamicpricingbackend/repositories/SpecialDayRepository.java
@@ -0,0 +1,40 @@
+package org.example.koudynamicpricingbackend.repositories;
+
+import org.example.koudynamicpricingbackend.entities.SpecialDay;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public interface SpecialDayRepository extends JpaRepository<SpecialDay, Long> {
+    // Query has three part;
+    //first checking country scope, if target country is null it will be global scope like new year
+    //second checking date for recurring
+    //third if there are any conflict take most pM
+
+    @Query("SELECT s FROM SpecialDay s WHERE " +
+            "(s.targetCountry IS NULL OR s.targetCountry = :depCountry OR s.targetCountry = :arrCountry) " +
+            "AND " +
+
+            // 2. TARİH KONTROLÜ
+            "( " +
+            // A) Tekrarlı DEĞİLSE (Örn: Kurban Bayramı 2025): Tam tarih aralığına bak.
+            "  (s.isRecurring = false AND :flightDate BETWEEN s.startDate AND s.endDate) " +
+            "  OR " +
+            // B) Tekrarlı İSE (Örn: Yılbaşı): Yılı yoksay, Ay ve Gün aralığına bak.
+            "  (s.isRecurring = true AND " +
+            "   (MONTH(:flightDate) * 100 + DAY(:flightDate)) BETWEEN " +
+            "   (MONTH(s.startDate) * 100 + DAY(s.startDate)) AND " +
+            "   (MONTH(s.endDate) * 100 + DAY(s.endDate)) " +
+            "  ) " +
+            ") " +
+
+            "ORDER BY s.priceMultiplier DESC LIMIT 1")
+    Optional<SpecialDay> findApplicableSpecialDay(
+            @Param("flightDate") LocalDate flightDate,
+            @Param("depCountry") String depCountry,
+            @Param("arrCountry") String arrCountry
+    );
+}
