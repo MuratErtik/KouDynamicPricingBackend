14d4a1c72129c6da61af8d9421fa3bcb2d5af5ef
Murat Ertik <dmuratertik1@gmail.com>
2025-12-04 02:07:12 -0800

    SpecialDay Exc has been added.

---
+++ src/main/java/org/example/koudynamicpricingbackend/exceptions/SpecialDayException.java
@@ -0,0 +1,7 @@
+package org.example.koudynamicpricingbackend.exceptions;
+
+public class SpecialDayException extends RuntimeException {
+    public SpecialDayException(String message) {
+        super(message);
+    }
+}
