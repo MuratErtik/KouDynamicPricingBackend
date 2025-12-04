2282fd295c7bf30e2323a15249ebe63591e45c21
Murat Ertik <dmuratertik1@gmail.com>
2025-12-04 03:19:44 -0800

    listing all special days.

---
+++ src/main/java/org/example/koudynamicpricingbackend/responses/SpecialDayResponse.java
@@ -0,0 +1,25 @@
+package org.example.koudynamicpricingbackend.responses;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class SpecialDayResponse {
+
+    private Long id;
+
+    private String name; // "Christmas", "Bayram", "Sömestr"
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    private Double priceMultiplier;
+
+
+    private String targetCountry;
+
+    private String targetCity;
+
+    private boolean isRecurring = false;
+}
