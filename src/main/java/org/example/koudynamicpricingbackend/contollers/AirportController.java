1faa2eb06e175764d162c676d99162353b15fddf
Murat Ertik <dmuratertik1@gmail.com>
2025-12-04 02:14:23 -0800

    Distinct countries and cities return.

--- src/main/java/org/example/koudynamicpricingbackend/contollers/AirportController.java
+++ src/main/java/org/example/koudynamicpricingbackend/contollers/AirportController.java
@@ -6,6 +6,7 @@ import org.example.koudynamicpricingbackend.services.AirportService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,6 +21,16 @@ public class AirportController {
     @GetMapping("/get-all")
     public ResponseEntity<List<AirportResponse>> getAllAirports() {
         return ResponseEntity.ok(airportService.getAllAirports());
+    }
+
+    @GetMapping("/countries")
+    public ResponseEntity<List<String>> getCountries() {
+        return ResponseEntity.ok(airportService.getAllCountries());
+    }
+
+    @GetMapping("/cities")
+    public ResponseEntity<List<String>> getCities(@RequestParam String country) {
+        return ResponseEntity.ok(airportService.getCitiesByCountry(country));
     }
 
 
