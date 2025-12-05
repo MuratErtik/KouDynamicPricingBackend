c1fba9609850cd3dea73f49ed882d72d30026e49
Murat Ertik <dmuratertik1@gmail.com>
2025-12-05 15:07:39 -0800

    deleteFlight added.

--- src/main/java/org/example/koudynamicpricingbackend/repositories/PriceHistoryRepository.java
+++ src/main/java/org/example/koudynamicpricingbackend/repositories/PriceHistoryRepository.java
@@ -1,5 +1,6 @@
 package org.example.koudynamicpricingbackend.repositories;
 
+import jakarta.transaction.Transactional;
 import org.example.koudynamicpricingbackend.entities.PriceHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,7 @@ import java.util.List;
 public interface PriceHistoryRepository extends JpaRepository<PriceHistory, Long> {
 
     List<PriceHistory> findByFlightIdOrderByChangeTimeAsc(Long flightId);
+
+    @Transactional
+    void deleteByFlightId(Long flightId);
 }
