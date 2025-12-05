cf58b6e02d7a3b2998e514fce20bf501416e6d20
Murat Ertik <dmuratertik1@gmail.com>
2025-12-05 07:49:24 -0800

    price history repo has been created.

---
+++ src/main/java/org/example/koudynamicpricingbackend/repositories/PriceHistoryRepository.java
@@ -0,0 +1,11 @@
+package org.example.koudynamicpricingbackend.repositories;
+
+import org.example.koudynamicpricingbackend.entities.PriceHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PriceHistoryRepository extends JpaRepository<PriceHistory, Long> {
+
+    List<PriceHistory> findByFlightIdOrderByChangeTimeAsc(Long flightId);
+}
