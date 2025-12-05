aa6df4a1893812ba56a705e3403bfa2e81a78dc6
Murat Ertik <dmuratertik1@gmail.com>
2025-12-05 07:49:00 -0800

    date query added.

--- src/main/java/org/example/koudynamicpricingbackend/repositories/FlightRepository.java
+++ src/main/java/org/example/koudynamicpricingbackend/repositories/FlightRepository.java
@@ -3,6 +3,13 @@ package org.example.koudynamicpricingbackend.repositories;
 import org.example.koudynamicpricingbackend.entities.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface FlightRepository extends JpaRepository<Flight, Long>, JpaSpecificationExecutor<Flight> {
+
+    @Query("SELECT f FROM Flight f WHERE f.departureTime BETWEEN :start AND :end")
+    List<Flight> findFlightsInDateRange(LocalDateTime start, LocalDateTime end);
 }
