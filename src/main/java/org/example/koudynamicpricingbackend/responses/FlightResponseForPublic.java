ee025ba11e4b5a3a3baa6a9bfa3e4c12da874383
Murat Ertik <dmuratertik1@gmail.com>
2025-12-09 05:38:46 -0800

    for public

---
+++ src/main/java/org/example/koudynamicpricingbackend/responses/FlightResponseForPublic.java
@@ -0,0 +1,26 @@
+package org.example.koudynamicpricingbackend.responses;
+
+import lombok.Data;
+import org.example.koudynamicpricingbackend.domains.FlightStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class FlightResponseForPublic {
+
+    private Long id;
+
+    private String flightNumber;
+
+    private AirportResponse departureAirport;
+
+    private AirportResponse arrivalAirport;
+
+    private LocalDateTime departureTime;
+
+    private LocalDateTime arrivalTime;
+
+    private BigDecimal currentPrice;
+
+}
