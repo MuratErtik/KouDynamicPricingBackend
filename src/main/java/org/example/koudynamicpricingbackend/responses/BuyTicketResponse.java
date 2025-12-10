05225d207a50fef4cb54c14a541d913bf8e6b662
Murat Ertik <dmuratertik1@gmail.com>
2025-12-10 18:58:12 +0300

    Buy Ticker response has been added.

---
+++ src/main/java/org/example/koudynamicpricingbackend/responses/BuyTicketResponse.java
@@ -0,0 +1,26 @@
+package org.example.koudynamicpricingbackend.responses;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+public class BuyTicketResponse {
+
+    private String pnr;
+
+    private String flightNumber;
+    private String route;              // Istanbul (IST) -> Ankara (ESB)
+    private LocalDateTime departureTime;
+    private LocalDateTime arrivalTime;
+
+    private BigDecimal totalPrice;
+    private int passengerCount;
+
+    private List<TicketDetailResponse> tickets;
+}
+
