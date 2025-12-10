f66bc13bd4847c5811802c922f31e60b137aed59
Murat Ertik <dmuratertik1@gmail.com>
2025-12-10 22:30:26 +0300

    findByIdentityNumber method has been added.

--- src/main/java/org/example/koudynamicpricingbackend/repositories/PassengerRepository.java
+++ src/main/java/org/example/koudynamicpricingbackend/repositories/PassengerRepository.java
@@ -3,5 +3,10 @@ package org.example.koudynamicpricingbackend.repositories;
 import org.example.koudynamicpricingbackend.entities.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PassengerRepository extends JpaRepository<Passenger, Long> {
+
+    // it going to find id no which hashed already
+    Optional<Passenger> findByIdentityNumber(String identityNumber);
 }
