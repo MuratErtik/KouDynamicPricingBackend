283d5c8dd09100c0159e30949a3997873838498a
Murat Ertik <dmuratertik1@gmail.com>
2025-12-10 17:25:03 +0300

    seat exception has been created.

---
+++ src/main/java/org/example/koudynamicpricingbackend/exceptions/SeatException.java
@@ -0,0 +1,7 @@
+package org.example.koudynamicpricingbackend.exceptions;
+
+public class SeatException extends RuntimeException {
+    public SeatException(String message) {
+        super(message);
+    }
+}
