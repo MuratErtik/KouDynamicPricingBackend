9b08bc831c2afdaaf32516c9c8e77eac448d7334
Murat Ertik <dmuratertik1@gmail.com>
2025-12-10 17:25:43 +0300

    findByFlightAndSeatNumberIn method has been added.

--- src/main/java/org/example/koudynamicpricingbackend/repositories/SeatRepository.java
+++ src/main/java/org/example/koudynamicpricingbackend/repositories/SeatRepository.java
@@ -1,13 +1,17 @@
 package org.example.koudynamicpricingbackend.repositories;
 
+import org.example.koudynamicpricingbackend.entities.Flight;
 import org.example.koudynamicpricingbackend.entities.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SeatRepository extends JpaRepository<Seat, Long> {
 
     List<Seat> findByFlightIdOrderByIdAsc(Long flightId);
 
     void deleteByFlightId(Long flightId);
+
+    Optional<List<Seat>> findByFlightAndSeatNumberIn(Flight flight, List<String> seats);
 }
