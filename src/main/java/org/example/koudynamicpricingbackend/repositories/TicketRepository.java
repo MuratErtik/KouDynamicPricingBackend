c4fd0413ed00e405aaed5a31fc3554ea189c9534
Murat Ertik <dmuratertik1@gmail.com>
2025-12-13 19:43:04 +0300

    findByPnrAndPassenger method has been added.

--- src/main/java/org/example/koudynamicpricingbackend/repositories/TicketRepository.java
+++ src/main/java/org/example/koudynamicpricingbackend/repositories/TicketRepository.java
@@ -1,8 +1,14 @@
 package org.example.koudynamicpricingbackend.repositories;
 
+import org.example.koudynamicpricingbackend.entities.Passenger;
 import org.example.koudynamicpricingbackend.entities.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
     boolean existsByPnr(String pnr);
+
+    Optional<Ticket> findByPnrAndPassenger(String pnr, Passenger passenger);
 }
