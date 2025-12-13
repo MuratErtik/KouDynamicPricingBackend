4945d08cc4b9e1d0ebf2190eeb0536eccb0a1805
Murat Ertik <dmuratertik1@gmail.com>
2025-12-13 19:42:36 +0300

    passenger exception has been created.

---
+++ src/main/java/org/example/koudynamicpricingbackend/exceptions/PassengerException.java
@@ -0,0 +1,7 @@
+package org.example.koudynamicpricingbackend.exceptions;
+
+public class PassengerException extends RuntimeException {
+    public PassengerException(String message) {
+        super(message);
+    }
+}
