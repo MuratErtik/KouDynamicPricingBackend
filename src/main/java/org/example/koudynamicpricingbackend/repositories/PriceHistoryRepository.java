2353cc63f4f0e7cf24cbb8f54da3e656f0a913e6
Murat Ertik <dmuratertik1@gmail.com>
2025-12-14 12:42:31 +0300

    findAllByFlight method has been created.

--- src/main/java/org/example/koudynamicpricingbackend/repositories/PriceHistoryRepository.java
+++ src/main/java/org/example/koudynamicpricingbackend/repositories/PriceHistoryRepository.java
@@ -1,10 +1,12 @@
 package org.example.koudynamicpricingbackend.repositories;
 
 import jakarta.transaction.Transactional;
+import org.example.koudynamicpricingbackend.entities.Flight;
 import org.example.koudynamicpricingbackend.entities.PriceHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PriceHistoryRepository extends JpaRepository<PriceHistory, Long> {
 
@@ -12,4 +14,7 @@ public interface PriceHistoryRepository extends JpaRepository<PriceHistory, Long
 
     @Transactional
     void deleteByFlightId(Long flightId);
+
+    Optional<List<PriceHistory>> findAllByFlight(Flight flight);
+
 }
