d049914965e5a087d7d6ba3b16bdda81c668e85e
Murat Ertik <dmuratertik1@gmail.com>
2025-12-14 18:24:03 +0300

    findAllByFlightOrderByIdAsc method has been created.

--- src/main/java/org/example/koudynamicpricingbackend/repositories/TicketRepository.java
+++ src/main/java/org/example/koudynamicpricingbackend/repositories/TicketRepository.java
@@ -1,9 +1,12 @@
 package org.example.koudynamicpricingbackend.repositories;
 
+import org.example.koudynamicpricingbackend.entities.Flight;
 import org.example.koudynamicpricingbackend.entities.Passenger;
+import org.example.koudynamicpricingbackend.entities.Seat;
 import org.example.koudynamicpricingbackend.entities.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
@@ -11,4 +14,7 @@ public interface TicketRepository extends JpaRepository<Ticket, Long> {
     boolean existsByPnr(String pnr);
 
     Optional<Ticket> findByPnrAndPassenger(String pnr, Passenger passenger);
+
+    Optional<List<Ticket>> findAllByFlightOrderByIdAsc(Flight flight);
+
 }
