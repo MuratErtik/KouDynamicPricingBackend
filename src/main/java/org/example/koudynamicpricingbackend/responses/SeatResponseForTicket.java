130776e3fe53c7dba2d9ce7b01d26186da282ded
Murat Ertik <dmuratertik1@gmail.com>
2025-12-14 18:25:22 +0300

    ticket dto's have been created.

---
+++ src/main/java/org/example/koudynamicpricingbackend/responses/SeatResponseForTicket.java
@@ -0,0 +1,12 @@
+package org.example.koudynamicpricingbackend.responses;
+
+import lombok.Data;
+
+@Data
+public class SeatResponseForTicket {
+
+    private Long id;
+
+
+    private String seatNumber;
+}
